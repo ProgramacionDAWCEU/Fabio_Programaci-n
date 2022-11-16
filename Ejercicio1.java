9073bb159263a684275c4328154061437450bf09
Fabio Rodríguez <65049200+Pachec00@users.noreply.github.com>
2022-11-16 17:28:39 +0100

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,19 @@
+package programas;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		Integer[] listaNotas = new Integer[] {7, 9, 8, 4, 0, 6, 3, 4, 1, 7, 3, 1, 9, 10}; 
+		
+		Integer contador = 0;
+		for ( int i = 0; i < listaNotas.length; i++ ) {
+			if ( listaNotas[i] < 5 ) {
+				contador++;
+			}
+		}
+		
+		System.out.println("El numero de suspensos es : " + contador);
+
+	}
+
+}
