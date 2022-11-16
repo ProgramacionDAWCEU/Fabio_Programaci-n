2c2f8112ad58dbdc429f914b7073f01d63383bc8
pgrobot <118376708+pgrobot@users.noreply.github.com>
2022-11-16 18:45:10 +0100

    Update Ejercicio3.java

--- Ejecicios_repaso_Tema3/Ejercicio3.java
+++ Ejecicios_repaso_Tema3/Ejercicio3.java
@@ -5,28 +5,32 @@ import java.util.Scanner;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Integer[] listaNotas = new Integer[] {7, 9, 8, 4, 0, 6, 3, 4, 1, 7, 3, 1, 9, 10}; 
-		
-		System.out.println("¿Que nota buscas?");
-		Integer nota = scanner.nextInt();
-		Integer pos = 0;
-		
-		
-		for ( int i = 0; i < listaNotas.length; i++) {
-			if ( nota != listaNotas[i] ) {
-				System.out.println("No");
+	Scanner scanner = new Scanner(System.in);
+			Integer[] listaNotas = new Integer[] {7, 9, 8, 4, 0, 6, 3, 4, 1, 7, 3, 1, 9, 10}; 
 			
-			}else if ( nota == listaNotas[i]) {
-				pos = i;
-				System.out.println("si " + i);
-				break;
+			System.out.println("Â¿Que nota buscas?");
+			Integer nota = scanner.nextInt();
+			Integer pos = 0;
+			Boolean t=null;
+			
+			for ( int i = 0; i < listaNotas.length; i++) {
+				if ( nota != listaNotas[i] ) {
+					t=false;
+				
+				}else if ( nota == listaNotas[i]) {
+					pos = i;
+					t=true;
+					break;
+				}
 			}
-		}
-		
-		
-		
-		scanner.close();
+			if(t==true) {
+				System.out.println("Si, existe la posi es "+ pos);
+			}else{
+				System.out.println("No existe");
+			}
+			
+			
+			scanner.close();
 		
 		
 
